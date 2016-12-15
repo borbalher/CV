077f4488f4855590f1e293013f338feddaf0762d
Boris Ballester Hernández <borbalher@gmail.com>
2016-12-15 19:33:15 +0000

    Third-party modules
    Added Skrollr with plug-ins and modernizr via npm

--- app.js
+++ app.js
@@ -7,9 +7,29 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+
+app.get('/css/normalize.css', function (req, res) {
+   res.sendFile(__dirname + '/node_modules/normalize.css/normalize.css');
+});
+
+app.get('/js/skrollr.js', function (req, res) {
+   res.sendFile(__dirname + '/node_modules/skrollr/dist/skrollr.min.js');
+});
+
+app.get('/js/skrollr-ie.js', function (req, res) {
+   res.sendFile(__dirname + '/node_modules/skrollr-ie/dist/skrollr.ie.min.js');
+});
+
+app.get('/js/skrollr-menu.js', function (req, res) {
+   res.sendFile(__dirname + '/node_modules/skrollr-menu/dist/skrollr.menu.min.js');
+});
+
+app.get('/js/skrollr-stylesheets.js', function (req, res) {
+   res.sendFile(__dirname + '/node_modules/skrollr-stylesheets/dist/skrollr.stylesheets.min.js');
 });
 
 app.listen(3000, function () {
