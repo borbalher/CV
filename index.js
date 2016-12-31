d8528023dd56012df4017f607f14185a5c293090
Boris Ballester Hernández <borbalher@gmail.com>
2016-12-31 04:11:25 +0000

    Changes in text

--- index.js
+++ index.js
@@ -8,20 +8,92 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hi! I\'m Boris Ballester. Welcome to my site!',
+
+
+  var education = {
+    "course_name": ["Computer Engineering Degree:  computation and artificial intelligence","Professional Training: Interactive Applications Development in 4th generation environments and CASE tools"],
+    "course_institution": ["Universidad de La Laguna","IES Domingo P&eacute;rez Minik"],
+    "course_begin":  [2011,2009],
+    "course_end": [2016,2011]
+  };
+
+  var cumplimentary_education = {
+    'course_name': [
+      "Introduction to Web Development using CAKEPHP",
+      "Google Act&iacute;vate: Developing Mobile Apps",
+      "Google Act&iacute;vate: Cloud Computing",
+      "Google Act&iacute;vate: Web Analytics",
+      "Google Act&iacute;vate Basic Marketing",
+      "LaunchTF: Startup's pre-acceleration program",
+      "Orientation and Introduction to Web Design Jobs",
+      "E-commerce: an alternative for sales"
+    ],
+    'course_institution': [
+      "Fundaci&oacute;n General de la Universidad de La Laguna",
+      "Universidad Complutense de Madrid",
+      "Escuela de Organizaci&oacute;n Industrial",
+      "Escuela de Organizaci&oacute;n Industrial",
+      "Instituto de Econom&iacute;a Internacional",
+      "Interactive Advertising Bureau",
+      "Fundaci&oacute;n General de la Universidad de La Laguna",
+      "C&aacute;mara de Comercio de Santa Cruz de Tenerife"
+    ],
+    'course_hours': [10,40,40,40,40,70,4,4],
+    'course_year': [2013,2014,2014,2014,2014,2014,2016,2016]
+  };
+
+var skills ={
+  'texts': ["Through the years I\'ve been in touch with",
+                 "but, in the present, I feel comfortable with the following:",
+                 "Those are the ones that I use the most but there are other programming languages that I can use to make awesome things:",
+                 "and to a lesser extent",
+                 "I just only need a"
+  ],
+  'boldtexts': ["many programming languages",
+               "C, C++, C#, .NET, Java",
+               "Prolog, Ruby and Python.",
+               "little adaptation period to switch languages."
+  ],
+  'skills': ["XAMP apps","MEAN stack","SCRUM agile dev","Git/Gitflow","Adobe Photoshop & Illustrator","HTML,CSS & Javascript"]
+};
+
+var objectives ={
+  'texts': ["as computer engineer in a laboral environment.","of an IT enterprise.","At last but not least:"],
+  'boldtexts': ["Earning experience","Learn the workflow","Mastering my skills or learn some new ones","do my best!"]
+};
+
+var whoami ={
+  'texts': ["I'm ",
+            ", a young guy from a little paradise called ",
+            " and located in ",
+            ". I've been surrounded by ",
+            " all my life and is one of my passions along with ",
+            " and "
+  ],
+  'boldtexts': ["Boris Ballester",
+                "Canary Islands",
+                "Spain",
+                "technology",
+                "digital art",
+                "creating things.",
+                " For me, doing almost anything is not impossible. It's just a matter of time!"
+  ]
+};
+
+res.render('index', {   'title': 'Hi! I\'m Boris Ballester. Welcome to my site!',
                         'about': 'About me', 'work': 'Work', 'contact': 'Contact me',
                         'intro': 'Computer engineering + design',
                         'whoamititle' : 'Who am I?',
-                        'whoamidesc' : 'I\'m <span class="bold">Boris Ballester</span>, a young guy from a little paradise called <span class="bold">Canary Islands</span> and located in <span class="bold">Spain</span>. I\'ve been surrounded by <span class="bold">technology</span> all my life and is one of my passions along with <span class="bold">digital art</span> and <span class="bold">creating</span> things. <span class="bold">For me, do anything isn\'t impossible. It\'s just a matter of time!</span>',
+                        'whoami' : whoami,
                         'objectivestitle' : 'My career objectives',
-                        'objectivesdesc' : '<li><span class="bold">Earning experience</span> as computer engineer in a laboral environment.</li><li><span class="bold">Learn the workflow</span> of an IT enterprise.</li><li><span class="bold">Mastering my skills</span> or <span class="bold">learn some new ones.</span></li><li>At last but not least: <span class="bold">do my best!</span>',
-                        'skillstitle' : 'My skills <i id="rocket" class="fa fw fa-rocket" aria-hidden="true"></i>',
-                        'skillsdesc' : '<p>Through the years I\'ve been in touch with <span class="bold">many programming languages</span> but, in the present, I feel comfortable with the following:</p><ul><li><span class="bold">XAMP</span> applications.</li><li><span class="bold">MEAN</span> stack.</li><li><span class="bold">SCRUM</span> agile development.</li><li><span class="bold">Git/Gitflow</span> version control.</li><li><span class="bold">Adobe Photoshop and Illustrator</span>.</li><li><span class="bold">HTML, CSS & Javascript.</span></li></ul><p>Those are the ones that I use the most but there are other programming languages that I can use to make awesome things: <span class="bold">C, C++, C#, .NET, Java</span> and to a lesser extent <span class="bold">Prolog, Ruby and Python.</span></p> <p>I just only need a <span class="bold">little adaptation period to switch languages.</span></p>',
+                        'objectives' : objectives,
+                        'skillstitle' : 'My skills',
+                        'skills' : skills,
                         'educationtitle' : 'Education',
-                        'educationdesc' : '<ul><li><span class="bold">Computer Engineering Degree:  computation and artificial intelligence</span> — <span class="italic">Universidad de La Laguna </span> [2011-2016]</li><li><span class="bold">Professional Training: Interactive Applications Development in 4th generation environments and CASE tools</span> — <span class="italic">IES Domingo P&eacute;rez Minik</span> [2009-2011]</li></ul>',
-                        'cumplimentaryeducationtitle' : '<i class="fa fw fa-book"></i> Cumplimentary education',
-                        'cumplimentaryeducationdesc' : '<ul><li><span class="bold">Introduction to Web Development using CAKEPHP</span>  — <span class="italic">Fundaci&oacute;n General de la Universidad de La Laguna </span> [2013] [10 hours]</li><li><span class="bold">LaunchTF: Startup\'s pre-acceleration program </span> —  <span class="italic">Fundaci&oacute;n General de la Universidad de La Laguna</span> [2015] [70 hours]</li><li><span class="bold">Google Act&iacute;vate: Developing Mobile Apps</span> -  <span class="italic">Universidad Complutense de Madrid</span> [2014] [40 hours]</li><li><span class="bold">Google Act&iacute;vate: Cloud Computing</span> - <span class="italic">Escuela de Organizaci&oacute;n Industrial</span> [2014] [40 hours]</li><li><span class="bold">Google Act&iacute;vate: Web Analytics - <span class="italic">Escuela de Organizaci&oacute;n Industrial [2014] [40 hours]</li><li><span class="bold">Google Act&iacute;vate Basic Marketing -  <span class="italic">Escuela de Organizaci&oacute;n Industrial</span> [2014] [40 hours]</li><li><span class="bold">LaunchTF: Startup\'s pre-acceleration program </span> —  <span class="italic">Fundaci&oacute;n General de la Universidad de La Laguna</span> [2015] [70 hours]</li><li><span class="bold">Orientation and Introduction to Web Design</span>  — <span class="italic"> Fundaci&oacute;n General de la Universidad de La Laguna</span> [2016] [4 hours]</li><li><span class="bold">E-commerce: an alternative for sales</span> - <span class="italic">C&aacute;mara de Comercio de Santa Cruz de Tenerife</span> [2016] [4 hours]</li></ul>'
-                      });
+                        'education' : education,
+                        'cumplimentaryeducationtitle' : ' Cumplimentary education',
+                        'cumplimentaryeducation' : cumplimentary_education
+                    });
 });
 
 app.get('/css/normalize.css', function (req, res) {
